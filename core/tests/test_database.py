27355ae80d1dfe53a3394b80b9fa45638730070d
ashkantvn <ashkantavangar7@gmail.com>
2025-11-28 12:00:27 +0330

    create test for user

--- core/tests/test_database.py
+++ core/tests/test_database.py
@@ -1,6 +1,12 @@
 from sqlalchemy import text
+from database import engine
 
 class TestDatabase:
     def test_connection(self, db_session):
         result = db_session.execute(text("SELECT 1")).scalar()
         assert result == 1
+
+    def test_user_table_exists(self):
+        inspector = engine.inspect(engine)
+        tables = inspector.get_table_names()
+        assert "user" in tables
